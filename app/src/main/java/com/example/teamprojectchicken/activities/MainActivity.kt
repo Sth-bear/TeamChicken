5732e79e25df98f36c9ae5e8ae2639f84fd43bf5
HOCRUX\penci <pencil2486@gmail.com>
2024-04-22 12:03:04 +0900

    ViewBinding

--- app/src/main/java/com/example/teamprojectchicken/activities/MainActivity.kt
+++ app/src/main/java/com/example/teamprojectchicken/activities/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.teamprojectchicken.R
+import com.example.teamprojectchicken.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
