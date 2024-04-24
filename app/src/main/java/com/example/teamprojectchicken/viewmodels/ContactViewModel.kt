425fd5608d835239ba18cfde6cf2a80f85eba05f
HOCRUX\penci <pencil2486@gmail.com>
2024-04-24 11:38:42 +0900

    [Fix]LiveData

--- app/src/main/java/com/example/teamprojectchicken/viewmodels/ContactViewModel.kt
+++ app/src/main/java/com/example/teamprojectchicken/viewmodels/ContactViewModel.kt
@@ -1,12 +1,16 @@
 package com.example.teamprojectchicken.viewmodels
 
+import android.util.MutableInt
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 class ContactViewModel(): ViewModel() {
     private var data = MutableLiveData<Boolean>()
+    private var viewType = MutableLiveData(1)
     val liveData : LiveData<Boolean>
         get() = data
+
+    val _viewType : LiveData<Int> get() = viewType
 
     fun getData(boolean: Boolean) {
         data.value = boolean
@@ -15,4 +19,17 @@ class ContactViewModel(): ViewModel() {
     fun setData(input:Boolean){
         data.value = input
     }
+
+    fun getType() {
+        viewType.value
+    }
+
+    fun setType() {
+        if (viewType.value == 1) {
+            viewType.value = 2
+        } else {
+            viewType.value = 1
+        }
+    }
+
 }
