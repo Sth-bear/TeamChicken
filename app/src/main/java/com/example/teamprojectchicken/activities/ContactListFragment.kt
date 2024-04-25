0270a93ecf8618d3481c6150542ac4ab86680129
bora <bora44144@gmail.com>
2024-04-25 11:06:48 +0900

    [Feat] 입력한 전화번호가 0으로 시작하는 11자리 이하의 숫자가 아닌 경우 토스트메시지 수정

--- app/src/main/java/com/example/teamprojectchicken/activities/ContactListFragment.kt
+++ app/src/main/java/com/example/teamprojectchicken/activities/ContactListFragment.kt
@@ -5,7 +5,6 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
-import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
@@ -22,7 +21,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
