cbcdb6e97b649d814bad2e68a45d4bcd02884f0e
bonggyulim <qhdrbdhkdwks@gmail.com>
2024-04-25 12:41:58 +0900

    [Feat]HeartFragment에서 Detail로 가는 기능 구현

--- app/src/main/java/com/example/teamprojectchicken/activities/RootHeart.kt
+++ app/src/main/java/com/example/teamprojectchicken/activities/RootHeart.kt
@@ -5,21 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.setFragmentResultListener
 import com.example.teamprojectchicken.R
-import com.example.teamprojectchicken.data.Contact
 
-class RootFragment : Fragment() {
+class RootHeart : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+
         }
     }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_root_heart, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (childFragmentManager.findFragmentById(R.id.root_frag)==null){
+        if (childFragmentManager.findFragmentById(R.id.root2_frag)==null){
             childFragmentManager.beginTransaction().apply {
-                replace(R.id.root_frag, ContactListFragment())
+                replace(R.id.root2_frag, HeartFragment())
                 addToBackStack(null)
                 commit()
             }
@@ -29,17 +36,9 @@ class RootFragment : Fragment() {
     override fun onPause() {
         super.onPause()
         childFragmentManager.beginTransaction().apply {
-            replace(R.id.root_frag, ContactListFragment())
+            replace(R.id.root2_frag, HeartFragment())
             addToBackStack(null)
             commit()
         }
-    }
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_root, container, false)
     }
 }
