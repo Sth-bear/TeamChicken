a3316421498885168122bd2d1420fe50e5082bfc
HOCRUX\penci <pencil2486@gmail.com>
2024-04-26 16:44:06 +0900

    [Fix]연락처 추가시 ì새로고침 í문제 해결

--- app/src/main/java/com/example/teamprojectchicken/activities/ContactListFragment.kt
+++ app/src/main/java/com/example/teamprojectchicken/activities/ContactListFragment.kt
@@ -5,7 +5,6 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
